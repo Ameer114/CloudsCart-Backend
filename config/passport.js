28233fa846f14a7d5848cd763a33906b5946fd80
Ameer Nagarasi <124485878+Ameer114@users.noreply.github.com>
2025-10-04 20:48:02 +0530

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -4,7 +4,7 @@ import passport from 'passport';
 passport.use(new GoogleStrategy({
     clientID:     process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SEC_KEY,
-    callbackURL: "http://localhost:4000/api/auth/google/callback",
+    callbackURL: "https://cloudscart-backend.onrender.com/api/auth/google/callback",
     passReqToCallback   : true
   },
   function(request, accessToken, refreshToken, profile, done) {
